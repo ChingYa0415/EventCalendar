e356636e5b53dcc8301dea6eaeb245129da9f9b9
Angus Liu <angus09855069@gmail.com>
2023-07-21 20:44:29 +0800

    1. 新增CalendarView、EventCalendarView 2. 程式碼組織化

---
+++ EventCalendar/View/Calendar/CalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarView.swift
+//  EventCalendar
+//
+//  Created by Angus on 2023/7/21.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarView()
+}
---
+++ EventCalendar/View/EventCalendar/EventCalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  EventCalendarView.swift
+//  EventCalendar
+//
+//  Created by Angus on 2023/7/21.
+//
+
+import SwiftUI
+
+struct EventCalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventCalendarView()
+}
